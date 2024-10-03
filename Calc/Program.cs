720cca9889ade9ed9f478f780585033ac1676957
Ilyuh_dr01@top-academy.ru <Ilyuh_dr01@top-academy.ru>
2024-10-03 11:26:56 -0400

    Factorial and  Calculator in C# DONE

---
+++ Calc/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calc
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+
+			while (true)
+			{
+				Console.WriteLine("Введите арифмитическое выражение: ");
+				string input = Console.ReadLine();
+				string[] operands = input.Split('+', '-', '*', '/');
+				double a = Convert.ToDouble(operands[0]);
+				double b = Convert.ToDouble(operands[1]);
+
+				if (input.Contains("+")) Console.WriteLine($"{a} + {b} = {a + b}");
+				else if (input.Contains("-")) Console.WriteLine($"{a} - {b} = {a - b}");
+				else if (input.Contains("*")) Console.WriteLine($"{a} * {b} = {a * b}");
+				else if (input.Contains("/")) if (b != 0) Console.WriteLine($"{a} / {b} = {a / b}"); else Console.WriteLine("Ошибка! Деление на ноль");
+				else Console.WriteLine("Ошибка! Неверная операция"); 
+			}
+		}
+	}
+}
---
+++ Factorial/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+
+namespace Factorial
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.WriteLine("Введите число: ");
+			int n = Convert.ToInt32(Console.ReadLine());
+			BigInteger f = 1;
+			for (int i = 1; i<= n; i++)
+			{
+				Console.Write(i + "! = ");
+				f *= i;
+                Console.WriteLine(f);
+			}
+		}
+	}
+}
