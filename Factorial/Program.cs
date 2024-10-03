7f56fa65e6006c5ca59a135df7d61dc820aa6d33
Ilyuh_dr01@top-academy.ru <Ilyuh_dr01@top-academy.ru>
2024-10-03 12:41:14 -0400

    Factorial DONE

--- Factorial/Program.cs
+++ Factorial/Program.cs
@@ -15,12 +15,20 @@ namespace Factorial
 			Console.WriteLine("Введите число: ");
 			int n = Convert.ToInt32(Console.ReadLine());
 			BigInteger f = 1;
-			for (int i = 1; i<= n; i++)
+			try
 			{
-				Console.Write(i + "! = ");
-				f *= i;
-                Console.WriteLine(f);
+				for (int i = 1; i <= n; i++)
+				{
+					Console.Write(i + "! = ");
+					f *= i;
+					Console.WriteLine(f);
+				}
 			}
+			catch (Exception ex)
+			{
+				Console.WriteLine(ex.Message);
+			}
+			Console.WriteLine("Finita la comedia");
 		}
 	}
 }
