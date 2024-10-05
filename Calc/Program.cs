164d2e192da3350885490959d9fff9b11be3d0a6
Ilyuh_dr01@top-academy.ru <Ilyuh_dr01@top-academy.ru>
2024-10-05 11:11:55 -0400

    Snake homework DONE

--- Calc/Program.cs
+++ Calc/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.Specialized;
+using System.Data;
 using System.Linq;
+using System.Linq.Expressions;
 using System.Net.WebSockets;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,15 +19,37 @@ namespace Calc
 			{
 				Console.WriteLine("Введите арифмитическое выражение: ");
 				string input = Console.ReadLine();
-				string[] operands = input.Split('+', '-', '*', '/');
-				double a = Convert.ToDouble(operands[0]);
-				double b = Convert.ToDouble(operands[1]);
+				input = input.Replace('.', ','); //метод Replace() заменяет первый указанный символ вторым (или первую указанную строку второй) и возвращает измененную строку, при этом исходная строка остается неизменной
+				char[] delimiters = new char[] { '+', '-', '*', '/' };
+				string[] operands = input.Split(delimiters);
+				try
+				{
+					double a = Convert.ToDouble(operands[0]);
+					double b = Convert.ToDouble(operands[1]);
+					//метод Split(...), который принимает набор разделителей
+					//и возвращает токены ("разрезанную строку" по указанным разделителям
+					//в виде массива строк)
+					#region IFcalc
+					//if (input.Contains("+")) Console.WriteLine($"{a} + {b} = {a + b}");
+					//else if (input.Contains("-")) Console.WriteLine($"{a} - {b} = {a - b}");
+					//else if (input.Contains("*")) Console.WriteLine($"{a} * {b} = {a * b}");
+					//else if (input.Contains("/")) if (b != 0) Console.WriteLine($"{a} / {b} = {a / b}"); else Console.WriteLine("Ошибка! Деление на ноль");
+					//else Console.WriteLine("Ошибка! Неверная операция");  
+					#endregion
 
-				if (input.Contains("+")) Console.WriteLine($"{a} + {b} = {a + b}");
-				else if (input.Contains("-")) Console.WriteLine($"{a} - {b} = {a - b}");
-				else if (input.Contains("*")) Console.WriteLine($"{a} * {b} = {a * b}");
-				else if (input.Contains("/")) if (b != 0) Console.WriteLine($"{a} / {b} = {a / b}"); else Console.WriteLine("Ошибка! Деление на ноль");
-				else Console.WriteLine("Ошибка! Неверная операция"); 
+					//Метод IndexOfAny() возвращает индекс найденного символа, в данном случае символа из массива 'delimiters'
+					switch (input[input.IndexOfAny(delimiters)])
+					{
+						case '+': Console.WriteLine($"{a} + {b} = {a + b}"); break;
+						case '-': Console.WriteLine($"{a} - {b} = {a - b}"); break;
+						case '*': Console.WriteLine($"{a} * {b} = {a * b}"); break;
+						case '/': if (b != 0) Console.WriteLine($"{a} / {b} = {a / b}"); else Console.WriteLine("Error! Can't divide by zero"); break;
+					}
+				}
+				catch (Exception ex)
+				{
+                    Console.WriteLine(ex.Message);
+				}
 			}
 		}
 	}
---
+++ Snake/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snake
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			Console.OutputEncoding = System.Text.Encoding.UTF8;
+			Console.Title = "Snake";
+
+			//Инициализация позиции курсора внутри границ
+			int x = Console.WindowWidth / 2;
+			int y = Console.WindowHeight / 2;
+
+			//Скрываем стандартный курсор консоли
+			Console.CursorVisible = false;
+
+			//Методы для создания границ и нового курсора
+			DrawBorders();
+			DrawCursor(x, y);
+
+			while (true)
+			{
+				ConsoleKeyInfo keyInfo = Console.ReadKey(true);
+				ClearCursor(x, y);
+
+				switch (keyInfo.Key)
+				{
+					case ConsoleKey.W:
+					case ConsoleKey.UpArrow:
+						if (y > 1) y--; break;
+					case ConsoleKey.S:
+					case ConsoleKey.DownArrow:
+						if (y < Console.WindowHeight - 2) y++; break;
+					case ConsoleKey.D:
+					case ConsoleKey.RightArrow:
+						if (x < Console.WindowWidth - 2) x++; break;
+					case ConsoleKey.A:
+					case ConsoleKey.LeftArrow:
+						if (x > 1) x--; break;
+					case ConsoleKey.Escape:
+						return;
+					default: break;
+				}
+				//Рисуем курсор на новой позиции
+				DrawCursor(x, y);
+			}
+		}
+
+		static void DrawBorders()
+		{
+			int width = Console.WindowWidth;
+			int height = Console.WindowHeight;
+
+			//Верхняя и нижняя границы
+			for (int x = 0; x < width; x++)
+			{
+				Console.SetCursorPosition(x, 0);
+				Console.Write("━");
+				Console.SetCursorPosition(x, height - 1);
+				Console.Write("━");
+			}
+
+			//Левая и правая границы
+			for (int y = 0; y < height; y++)
+			{
+				Console.SetCursorPosition(0, y);
+				Console.Write("┃");
+				Console.SetCursorPosition(width - 1, y);
+				Console.Write("┃");
+			}
+
+			//Углы
+			Console.SetCursorPosition(0, 0);
+			Console.Write("┏");
+			Console.SetCursorPosition(width - 1, 0);
+			Console.Write("┓");
+			Console.SetCursorPosition(0, height - 1);
+			Console.Write("┗");
+			Console.SetCursorPosition(width - 1, height - 1);
+			Console.Write("┛");
+		}
+
+		static void DrawCursor(int x, int y)
+		{
+			Console.SetCursorPosition(x, y);
+			Console.Write("₷");
+		}
+
+		static void ClearCursor (int x, int y)
+		{
+			Console.SetCursorPosition(x, y);
+			Console.Write(" ");
+		}
+	}
+}
